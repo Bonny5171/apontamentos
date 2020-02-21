b0f2d1cc6fe6ce06917f2adec1541842567706ca
daniel kiesshau <luis.city@gmail.com>
2020-02-21 15:45:38 -0300

    TODO list

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log('OLA MUNDO')
+
+//TODO
+
+/*
+- autenticar 
+- pegar o token.
+- definir o objetos (Tarefas, Apontamento).
+- REST para criar as tarefas com a logica de inserir tbm os aponatamentos por semana.
+*/
